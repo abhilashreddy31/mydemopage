9bc9e82e1e8abfdc0079360154d38406a791dd50
abhilashreddy31 <kanamathareddyabhilash6@gmail.com>
2023-11-25 23:43:32 +0530

    final commit

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import memu from "../../asserts/menu.png"
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${showMenu ? 'active' : ''}`}>
+      <btn className="toggle-btn" onClick={toggleMenu}>
+          <img className='memu' src={memu}></img>
+        </btn>
       <div className="navbar-left">
-        <nav>
+        
+        <nav className={showMenu ? 'show-menu' : ''}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" onClick={toggleMenu}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about" onClick={toggleMenu}>
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/services">Schedules</Link>
+              <Link to="/services" onClick={toggleMenu}>
+                Schedules
+              </Link>
             </li>
             <li>
-              <Link to="/portfolio">Membership</Link>
+              <Link to="/portfolio" onClick={toggleMenu}>
+                Membership
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Pricing</Link>
+              <Link to="/contact" onClick={toggleMenu}>
+                Pricing
+              </Link>
             </li>
           </ul>
         </nav>
       </div>
       <div className="navbar-right">
-       
-        <Link to="/profile">Offers</Link>
-        <button>Courses</button>
+    
+        
+        <Link to="/profile" onClick={toggleMenu}>
+          Offers
+        </Link>
+        <button className='courses-btn' onClick={toggleMenu}>Courses</button>
       </div>
     </header>
   );
